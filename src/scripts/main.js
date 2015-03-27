74ee79970aea72e5629b860d7faf28ccd0487d25
ErikaLim <Erika.megan.lim@gmail.com>
2015-03-27 13:43:51 -0700

    set up columns in index.html

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+$("#search-form").on("submit", function (e) {
+  e.preventDefault();
+  searchAction($("#search-form input").val());
+});
+
+function searchAction(searchTerm) {
+  var query = searchTerm.replace(/\s/g, "+");
+  var url = "http://localhost:3000/search_yelp";
+  var req = {
+    url: url,
+    method: "post",
+    data:{
+      "s":query // params[:s]
+    }
+  };
+  
+  $.ajax(req)
+    .done(function (res) {
+      console.log(res);
+    })
+    .fail(function () {
+      throw "Search AJAX Failed";
+    });
+}
